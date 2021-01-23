9ad5d4be31e2efadc5c1bde0c56fec1ff5faa0ce
YutaUra <yuuta3594@outlook.jp>
2021-01-23 18:42:16 +0900

    fix some code

--- src/pick-begginer-leader.ts
+++ src/pick-begginer-leader.ts
@@ -1,4 +1,3 @@
-import { WebClient } from '@slack/web-api'
 import { BEGGINER_LEADER, CHANNELS } from './config'
 import {
   arrayAtLeastOne,
@@ -6,35 +5,20 @@ import {
   getJstDate,
   randomPickFromArray
 } from './lib'
-
-const client = new WebClient(process.env.SLACK_BOT_TOKEN)
-
-const getBegginerMembers = async () => {
-  const result = await client.conversations.members({
-    channel: CHANNELS.BEGGINER
-  })
-  if (!result.ok) {
-    await client.chat.postMessage({
-      channel: CHANNELS.BEGGINER,
-      text: '#begginer のメンバーを正常に取得できませんでした'
-    })
-    throw Error('メンバーを正常に取得できませんでした')
-  }
-  return result.members as string[]
-}
+import { getMembers, postMessage } from './slack-lib'
 
 export const handler = async () => {
-  const members = await getBegginerMembers()
+  const members = await getMembers({ channel: CHANNELS.BEGGINER })
   const withoutLeader = members.filter(member => member !== BEGGINER_LEADER)
   if (!arrayAtLeastOne(withoutLeader)) {
-    await client.chat.postMessage({
+    await postMessage({
       channel: CHANNELS.BEGGINER,
       text: '選択するメンバーがいません'
     })
     throw Error('選択するメンバーがいません')
   }
   const randomMember = randomPickFromArray(withoutLeader)
-  await client.chat.postMessage({
+  await postMessage({
     channel: CHANNELS.BEGGINER,
     text: `${formatDate(
       getJstDate(new Date()),
---
+++ src/slack-lib.ts
@@ -0,0 +1,28 @@
+import {
+  ChatPostMessageArguments,
+  ConversationsMembersArguments,
+  WebClient
+} from '@slack/web-api'
+import { CHANNELS } from './config'
+
+const client = new WebClient(process.env.SLACK_BOT_TOKEN)
+
+export const getMembers = async (options: ConversationsMembersArguments) => {
+  const result = await client.conversations.members(options)
+  if (!result.ok) {
+    await client.chat.postMessage({
+      channel: CHANNELS.BOT,
+      text: `\`\`\`${JSON.stringify(
+        options,
+        null,
+        2
+      )}\`\`\` のメンバーを正常に取得できませんでした`
+    })
+    throw Error('メンバーを正常に取得できませんでした')
+  }
+  return result.members as string[]
+}
+
+export const postMessage = async (options: ChatPostMessageArguments) => {
+  await client.chat.postMessage(options)
+}
